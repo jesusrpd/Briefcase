e92823ae8abbd6aa40e86f061093df9994f3bcc9
jesusrpd <jesusrodrigo881@gmail.com>
2022-07-11 04:25:40 -0500

    section service mobile

--- components/Servicio.jsx
+++ components/Servicio.jsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 
-export default function Service () {
+export default function Service ({title, img, desc}) {
   return(
-      
-        <div className="w-72 h-72 m-auto rounded bg-gradient-to-br from-red to-dark flex flex-col items-center justify-center my-4">
-          <Image
-            src="/SVG/code.svg"
-            width={48}
-            height={48}
-            alt="icon card"
-            className=" my-4"
-          />
-          <h3 className="text-white font-bold text-2xl my-4">Web develop</h3>
-          <p className="text-center text-white my-4">Desarrollo, planificación y desarrollo web para tu negocio, tienda etc.</p>
-        </div>
+    <div className="w-72 h-72 m-auto rounded bg-gradient-to-br from-red to-dark flex flex-col items-center justify-center my-4">
+      <Image
+        src={img}
+        width={48}
+        height={48}
+        alt="icon card"
+        className=" my-4"
+      />
+      <h3 className="text-white font-bold text-2xl my-4">{title}</h3>
+      <p className="text-center text-white my-4">{desc}</p>
+    </div>
   )
 }
--- components/Servicios.jsx
+++ components/Servicios.jsx
@@ -2,13 +2,13 @@ import Servicio from "./Servicio";
 
 export default function Service () {
   return(
-    <div className="w-100 h-screen">
+    <div className="w-100 min-h-screen">
       <h2 className="text-white text-center mb-4 text-2xl">Service</h2>
-        <div className="flex flex-col">
-          <Servicio/>
-          <Servicio/>
-          <Servicio/>
-          <Servicio/>
+        <div className="">
+          <Servicio title="Web develop" desc="Desarrollo, planificación y desarrollo web para tu negocio, tienda etc." img="/SVG/code.svg"/>
+          <Servicio title="Web design" desc="Diseño agradable y adaptable para todos los dispositivos." img="/SVG/design.svg"/>
+          <Servicio title="SEO" desc="Posicionamiento organico e inorganico de tu sitio o plataforma." img="/SVG/seo.svg"/>
+          <Servicio title="Web hosting" desc="Configuración y alojamiento en el servidor de tu sitio y creación de correos." img="/SVG/hosting.svg"/>
         </div>
     </div>
   )
